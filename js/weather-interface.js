59aa2d1564aa6848ca18ce2392bf348bc9f676e5
Chris Wainner and Janek Brandt <janek@>
2017-04-18 16:05:25 -0700

    add condition icons

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -2,14 +2,14 @@ var Weather = require('../js/weather.js').weatherModule;
 
 var displayOutput = function(output) {
   for (var i in output) {
-    $("#current ."+i).text(output[i]);
+    $("#current ."+i).html(output[i]);
   }
 }
 
 var displayForecast = function(output) {
   for (var i = 0; i < output.length; i++) {
     for (var j in output[i]) {
-      $("#"+(i+1)+" ."+j).text(output[i][j]);
+      $("#"+(i+1)+" ."+j).html(output[i][j]);
     }
   }
 }
--- js/weather.js
+++ js/weather.js
@@ -9,17 +9,24 @@ Weather.prototype.convertTemp = function(temp) {
   return output;
 };
 
+Weather.prototype.formatImg = function (src) {
+  return link = "<img src='http://openweathermap.org/img/w/"+src+".png'>";
+};
+
 Weather.prototype.getWeather = function(city, displayOutput) {
   var self = this;
   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=' + apiKey).then(function(response) {
     var condition = "";
+    var conditionIcon = "";
     response.weather.forEach(function(weather) {
       condition = condition + " / " + weather.main;
+      condidionIcon = weather.icon;
     })
     condition = condition.slice(3);
     var output = {};
     output["city"] = city;
     output["condition"] = condition;
+    output["conditionIcon"] = self.formatImg(condidionIcon);
     output["temp"] = self.convertTemp(response.main.temp);
     output["high"] = self.convertTemp(response.main.temp_max);
     output["low"] = self.convertTemp(response.main.temp_min);
@@ -35,13 +42,16 @@ Weather.prototype.getForecast = function (city, displayForecast) {
     var output = [];
     response.list.forEach(function(day) {
       var condition = "";
+      var conditionIcon = "";
       day.weather.forEach(function(weather) {
         condition = condition + " / " + weather.main;
+        condidionIcon = weather.icon;
       })
       condition = condition.slice(3);
       var dayForecast = {};
       dayForecast["day"] = moment(day.dt, "X").format("dddd");
       dayForecast["condition"] = condition;
+      dayForecast["conditionIcon"] = self.formatImg(condidionIcon);
       dayForecast["high"] = self.convertTemp(day.temp.max);
       dayForecast["low"] = self.convertTemp(day.temp.min);
       output.push(dayForecast);
